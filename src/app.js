15b8f69a6a9d4aac422d546493694b9bf3c1a0d1
Zero <silverfantacy@gmail.com>
2024-03-02 12:47:20 +0800

    新增快速回覆選項和Model切換

--- src/app.js
+++ src/app.js
@@ -19,6 +19,47 @@ const config = {
 };
 const bot = new line.Client(config);
 
+const quickReply = {
+  items: [
+    {
+      type: "action",
+      imageUrl: "https://th.bing.com/th?q=%e6%98%9f%e8%a1%97&w=42&h=42&c=7&rs=1&p=0&o=5&dpr=2&pid=1.7&mkt=zh-TW&cc=TW&setlang=zh-Hant&adlt=moderate&t=1",
+      action: {
+        type: "message",
+        label: "新話題",
+        text: "!新話題"
+      }
+    },
+    {
+      type: "action",
+      imageUrl: "https://th.bing.com/th?q=%e6%98%9f%e8%a1%97&w=42&h=42&c=7&rs=1&p=0&o=5&dpr=2&pid=1.7&mkt=zh-TW&cc=TW&setlang=zh-Hant&adlt=moderate&t=1",
+      action: {
+        type: "message",
+        label: "Modal查詢",
+        text: "!Modal查詢"
+      }
+    },
+    {
+      type: "action",
+      imageUrl: "https://th.bing.com/th?q=%e6%98%9f%e8%a1%97&w=42&h=42&c=7&rs=1&p=0&o=5&dpr=2&pid=1.7&mkt=zh-TW&cc=TW&setlang=zh-Hant&adlt=moderate&t=1",
+      action: {
+        type: "message",
+        label: "GPT-3.5-Turbo",
+        text: "!GPT-3.5-Turbo"
+      }
+    },
+    {
+      type: "action",
+      imageUrl: "https://th.bing.com/th?q=%e6%98%9f%e8%a1%97&w=42&h=42&c=7&rs=1&p=0&o=5&dpr=2&pid=1.7&mkt=zh-TW&cc=TW&setlang=zh-Hant&adlt=moderate&t=1",
+      action: {
+        type: "message",
+        label: "GPT-4-0125-Preview",
+        text: "!GPT-4-0125-Preview"
+      }
+    }
+  ]
+}
+
 // 建立 Express 實例
 const app = express();
 
@@ -48,16 +89,37 @@ async function handleEvent(event) {
   switch (requestString) {
     case '!新話題': {
       const directory = path.join(__dirname, 'users', hash);
-      fs.rmdir(directory, { recursive: true }, (err) => { // 使用 fs.rmdir 刪除資料夾
-        if (err) {
-          console.error(`Error deleting directory ${directory}: ${err}`)
-          return
-        }
-        return bot.replyMessage(event.replyToken, { type: 'text', text: '已經開啟新話題' })
-      })
+      // 修改資料夾名稱 最前面加上 [X]
+      const newDirectoryName = `[X]${hash}`;
+      fs.renameSync(directory, path.join(__dirname, 'users', newDirectoryName));
+      return bot.replyMessage(event.replyToken, { type: 'text', text: '已經開啟新話題', quickReply })
+
+      // fs.rmdir(directory, { recursive: true }, (err) => { // 使用 fs.rmdir 刪除資料夾
+      //   if (err) {
+      //     console.error(`Error deleting directory ${directory}: ${err}`)
+      //     return
+      //   }
+      //   return bot.replyMessage(event.replyToken, { type: 'text', text: '已經開啟新話題' })
+      // })
+      break;
+    }
+    case '!Modal查詢': {
+      const { currentModel } = await getCustomConfig(hash);
+      return bot.replyMessage(event.replyToken, { type: 'text', text: `目前使用: ${currentModel}` })
+      break;
+    }
+    case '!GPT-3.5-Turbo': {
+      saveCustomfile(hash, 'gpt-3.5-turbo');
+      return bot.replyMessage(event.replyToken, { type: 'text', text: `已切換至: gpt-3.5-turbo`, quickReply })
+      break;
+    }
+    case '!GPT-4-0125-Preview': {
+      saveCustomfile(hash, 'gpt-4-0125-preview');
+      return bot.replyMessage(event.replyToken, { type: 'text', text: `已切換至: gpt-4-0125-preview`, quickReply })
       break;
     }
     default: {
+      const { currentModel } = await getCustomConfig(hash);
       const previousMessages = await getPreviousMessages(hash);
       const messages = [
         { role: "system", content: '#zh-tw As a system, my role is to provide direct and concise answers in a contextual and conversational style. My responses should be casual and avoid opposition, warning, or summarization. I should not provide abstract or detailed explanations or trace the origins of a question.' },
@@ -71,7 +133,7 @@ async function handleEvent(event) {
           'Authorization': `Bearer ${OPENAI_API_KEY}`
         },
         json: {
-          model: `${OPENAI_MODEL}`,
+          model: `${currentModel}`,
           messages,
           temperature: 0.9,
           max_tokens: 256 * 1,
@@ -94,25 +156,13 @@ async function handleEvent(event) {
               fs.mkdirSync(directory);
             }
             const filePath = path.join(directory, fileName);
-            console.log(body)
             const content = `Question: ${message.text}\n\nAnswer: ${body.choices[0].message.content.trim()}`;
             fs.writeFileSync(filePath, content);
             resolve(bot.replyMessage(event.replyToken, {
               type: 'text',
               text: body.choices[0].message.content.trim(),
               notificationDisabled: true,
-              quickReply: {
-                items: [
-                  {
-                    type: "action",
-                    imageUrl: "https://th.bing.com/th?q=%e6%98%9f%e8%a1%97&w=42&h=42&c=7&rs=1&p=0&o=5&dpr=2&pid=1.7&mkt=zh-TW&cc=TW&setlang=zh-Hant&adlt=moderate&t=1",
-                    action: {
-                      type: "message",
-                      label: "新話題",
-                      text: "!新話題"
-                    }
-                  },]
-              }
+              quickReply
             }));
           }
         });
@@ -150,6 +200,41 @@ function getPreviousMessages(hash) {
   });
 }
 
+// 取得使用者使用的模型
+function saveCustomfile(hash, model) {
+  const directory = path.join(__dirname, 'custom');
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+  const fileName = `${hash}.txt`;
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+  }
+  const filePath = path.join(directory, fileName);
+  const content = `Model: ${model || OPENAI_MODEL}`;
+  fs.writeFileSync(filePath, content);
+}
+
+function getCustomConfig(hash) {
+  return new Promise((resolve, reject) => {
+    const directory = path.join(__dirname, 'custom');
+    if (!fs.existsSync(directory)) {
+      resolve(null);
+    } else {
+      const fileName = `${hash}.txt`;
+      const filePath = path.join(directory, fileName);
+      if (!fs.existsSync(filePath)) {
+        resolve(null);
+      } else {
+        const content = fs.readFileSync(filePath, 'utf-8');
+        const [model] = content.split('\n\n');
+        const currentModel = model.replace(/^Model: /, '').trim();
+        resolve({ currentModel });
+      }
+    }
+  }
+  )
+}
 
 // 啟動伺服器
 const port = process.env.PORT || 3000;
